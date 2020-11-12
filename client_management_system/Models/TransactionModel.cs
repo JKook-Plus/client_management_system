56c473e9e156c4871de77968f51ba04d921a62c8
JKook <JKookaburraSchool@gmail.com>
2020-11-13 09:09:58 +1100

    update to Transaction Model

--- client_management_system/Models/TransactionModel.cs
+++ client_management_system/Models/TransactionModel.cs
@@ -14,6 +14,10 @@ namespace client_management_system.Models
         [DataMember]
         public string ProductName { get; set; }
         [DataMember]
+        public string Variation { get; set; }
+
+
+        [DataMember]
         public float Cost { get; set; }
 
 
